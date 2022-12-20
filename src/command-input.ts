39c12a576570b85903dca854a49bd05fda5765a7
ator-dev <dominic.codedeveloper@gmail.com>
2022-12-20 00:32:14 +0000

    Respect user-defined operations
    - Revert hardcoded approach to tab-switching
    - Implement basic use of cached operations

--- src/command-input.ts
+++ src/command-input.ts
@@ -1,43 +1,3 @@
-let command = "";
-let input: HTMLElement | null = null;
-
-const commandEntered = async (commandNew: string, submit = false) => {
-	command = commandNew;
-	const getTabsInWindow = async () =>
-		(await chrome.tabs.query({ lastFocusedWindow: true }));
-	if (input) {
-		input.textContent = command;
-	}
-	if (command.startsWith("t")) {
-		if ((command.length > 1 && command.endsWith("t")) || submit) {
-			const tab = (await chrome.tabs.query({ highlighted: true, active: false }))[0];
-			chrome.tabs.update(tab.id as number, { active: true });
-			close();
-			return;
-		}
-		const tabs = await getTabsInWindow();
-		const tab = tabs.find(tab => tab.active) as chrome.tabs.Tab;
-		const tabSelected = tabs.find(tab => !tab.active && tab.highlighted) as chrome.tabs.Tab;
-		if (command.startsWith("tl")) {
-			const shift = (command.match(/l/g) as RegExpMatchArray).length;
-			chrome.tabs.update(tabs[(tab.index + shift) % tabs.length].id as number, {
-				highlighted: true,
-				active: false,
-			});
-			chrome.tabs.update(tabSelected.id as number, { highlighted: false });
-		} else if (command.startsWith("th")) {
-			const shift = (command.match(/h/g) as RegExpMatchArray).length;
-			chrome.tabs.update(tabs[(tabs.length - 1) + ((tab.index - (tabs.length - 1) - shift) % tabs.length)].id as number, {
-				highlighted: true,
-				active: false,
-			});
-			chrome.tabs.update(tabSelected.id as number, { highlighted: false });
-		}
-	}
-};
-
-commandEntered("t");
-
 addEventListener("keydown", event => {
 	if (event.key === "Escape") {
 		return;
@@ -56,6 +16,101 @@ addEventListener("keydown", event => {
 	}
 });
 
+let command = "";
+let input: HTMLElement | null = null;
+
+// TODO do not rely on storage being present
+let operations: Array<Operation> = [];
+chrome.storage.local.get("operations").then(storage => {
+	operations = storage["operations"];
+	operations.filter(operation => operation.operator === "prefix").forEach(operation => {
+		// TODO only use the appropriate prefix operation (corresponding to the shortcut used to open the popup)
+		commandEntered(operation.operands[1].label + command);
+	});
+});
+
+const getTabsInWindow = () =>
+	chrome.tabs.query({ lastFocusedWindow: true })
+;
+
+const evaluateExpression = (expressionString: string): number => {
+	const expression = expressionString.split(" ").map(node =>
+		node === "<length>"
+			? command.length
+			: [ "+", "-" ].includes(node) ? node : parseInt(node)
+	);
+	let result = 0;
+	let mode = "+";
+	expression.forEach(node => {
+		if (typeof node === "string") {
+			mode = node;
+		} else {
+			switch (mode) {
+			case "+": {
+				result += node;
+				break;
+			} case "-": {
+				result -= node;
+				break;
+			}}
+		}
+	});
+	return result;
+};
+
+const commandEntered = async (commandNew: string, submit = false) => {
+	command = commandNew;
+	if (input) {
+		input.textContent = command;
+	}
+	const operationsApplicable = operations
+		.filter(({ operator, operands }) => operator === "map" && (new RegExp(`\\b${operands[0].label}\\b`, "g")).test(command));
+	for (const { operands: [ pattern, action, replacement ] } of operationsApplicable) {
+		switch (action.label) {
+		case "tabs.highlight.shift": {
+			const tabs = await getTabsInWindow();
+			const tab = tabs.find(tab => tab.active) as chrome.tabs.Tab;
+			const tabSelected = tabs.find(tab => !tab.active && tab.highlighted);
+			const shift = evaluateExpression(action.arguments[0]);
+			const tabIndex = shift >= 0
+				? (tab.index + shift) % tabs.length
+				: (tabs.length - 1) + ((tab.index - (tabs.length - 1) + shift) % tabs.length);
+			chrome.tabs.update(tabs[tabIndex].id as number, {
+				highlighted: true,
+				active: false,
+			});
+			if (tabSelected) {
+				chrome.tabs.update(tabSelected.id as number, { highlighted: false });
+			}
+			break;
+		} case "tabs.activate.shift": {
+			const tabs = await getTabsInWindow();
+			const tab = tabs.find(tab => tab.active) as chrome.tabs.Tab;
+			const shift = evaluateExpression(action.arguments[0]);
+			const tabIndex = shift >= 0
+				? (tab.index + shift) % tabs.length
+				: (tabs.length - 1) + ((tab.index - (tabs.length - 1) + shift) % tabs.length);
+			chrome.tabs.update(tabs[tabIndex].id as number, { active: true });
+			break;
+		} case "tabs.activate.highlighted": {
+			const tab = (await chrome.tabs.query({ highlighted: true, active: false }))[0];
+			chrome.tabs.update(tab.id as number, { active: true });
+			break;
+		} case "meta.popup.close": {
+			close();
+			break;
+		} default: {
+			console.warn("unrecognised action", action);
+		}}
+		if (replacement !== undefined) {
+			command = replacement.label === "\"\"" ? "" : replacement.label;
+			if (input) {
+				input.textContent = command;
+			}
+		}
+	}
+};
+
 addEventListener("keyup", event => {
 	event.preventDefault();
 	if (event.key.length === 1 && event.key !== " " && !command.includes(event.key)) {
@@ -64,7 +119,7 @@ addEventListener("keyup", event => {
 });
 
 addEventListener("blur", () => {
-	//close();
+	close();
 });
 
 const popupInsert = (container: HTMLElement) => {
